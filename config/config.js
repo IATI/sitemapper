116f22a72f6e0134477f0ea9126e9029cd9bc1a9
akmiller01 <alex.k.miller@gmail.com>
2022-02-01 09:38:13 -0500

    Redis cache length and chunks

--- config/config.js
+++ config/config.js
@@ -7,4 +7,8 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV,
     NS_PER_SEC: 1e9,
     DSS_API_KEY: process.env.DSS_API_KEY,
+    REDIS_PORT: process.env.REDIS_PORT || 6379,
+    REDIS_CACHE_SEC: process.env.REDIS_CACHE_SEC || 86400,
+    REDIS_KEY: process.env.REDIS_KEY,
+    REDIS_HOSTNAME: process.env.REDIS_HOSTNAME,
 };
--- dss-sitemap/index.js
+++ dss-sitemap/index.js
@@ -1,5 +1,9 @@
 const axios = require('axios');
+const { aSetex, aGet, aExists } = require('../config/redis');
 const config = require('../config/config');
+
+const sitemapLimit = 50000;
+const cacheSeconds = 600;
 
 const encodeXML = function (str) {
     return str
@@ -19,20 +23,54 @@ const siteUrl = 'https://dev-ds-search.iatistandard.org/';
 const baseUrlSitemap =
     'https://dev-api.iatistandard.org/dss/activity/select?q=*:*&facet=true&facet.field=iati_identifier&facet.sort=index&facet.limit=-1';
 
-const getAllActivities = async () =>
-    axios
-        .get(baseUrlSitemap, axiosConfig)
-        .then((result) =>
-            result.data.facet_counts.facet_fields.iati_identifier.filter((d, i) => i % 2 === 0)
+const getAllActivities = async () => {
+    const activities = await axios.get(baseUrlSitemap, axiosConfig).then((result) => {
+        return result.data.facet_counts.facet_fields.iati_identifier.filter((d, i) => i % 2 === 0);
+    });
+    let cachePromises = [];
+    cachePromises.push(aSetex('dss_sitemap_count', cacheSeconds, activities.length));
+    const numChunks = Math.ceil(activities.length / sitemapLimit);
+    Array.from(Array(numChunks).keys()).forEach((chunkIndex) => {
+        const activitySlice = activities.slice(
+            chunkIndex * sitemapLimit,
+            (chunkIndex + 1) * sitemapLimit
         );
+        cachePromises.push(
+            aSetex(`dss_sitemap_chunk_${chunkIndex}`, cacheSeconds, JSON.stringify(activitySlice))
+        );
+    });
+    await Promise.all(cachePromises);
+    return activities;
+};
 
-const sitemapLimit = 50000;
+const getActivityCount = async () => {
+    if ((await aExists('dss_sitemap_count')) === 0) {
+        const activities = await getAllActivities();
+        return activities.length;
+    } else {
+        const activityCount = await aGet('dss_sitemap_count');
+        return activityCount;
+    }
+};
+
+const getActivitySlice = async (chunkIndex) => {
+    if ((await aExists(`dss_sitemap_chunk_${chunkIndex}`)) === 0) {
+        const activities = await getAllActivities();
+        const activitySlice = activities.slice(
+            chunkIndex * sitemapLimit,
+            (chunkIndex + 1) * sitemapLimit
+        );
+        return activitySlice;
+    } else {
+        const activitySlice = await aGet(`dss_sitemap_chunk_${chunkIndex}`);
+        return JSON.parse(activitySlice);
+    }
+};
 
 const getSitemapIndex = async () => {
     let sitemapIndexString =
         '<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-    const activities = await getAllActivities();
-    const activityCount = activities.length;
+    const activityCount = await getActivityCount();
     const sitemapExtent = Math.ceil(activityCount / sitemapLimit);
     sitemapIndexString += Array.from(Array(sitemapExtent).keys())
         .map((i) => `<sitemap><loc>${siteUrl}sitemap-${i}.xml</loc></sitemap>`)
@@ -44,9 +82,8 @@ const getSitemapIndex = async () => {
 const getSingleSitemap = async (sitemapNumber) => {
     let sitemapString =
         '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">';
-    const activities = await getAllActivities();
+    const activities = await getActivitySlice(sitemapNumber);
     sitemapString += activities
-        .slice(sitemapNumber * sitemapLimit, (sitemapNumber + 1) * sitemapLimit)
         .map((d) => `<url><loc>${siteUrl}activity/${encodeXML(encodeURI(d))}</loc></url>`)
         .join('');
     sitemapString += '</urlset>';
