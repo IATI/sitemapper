d392741238605d80ba591ed336b0253e641d06b0
akmiller01 <alex.k.miller@gmail.com>
2022-04-01 15:01:49 -0400

    Validator dynamic sitemap

---
+++ validator-sitemap/index.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+const config = require('../config/config');
+
+const additionalPages = ['', 'about', 'validate', 'organisations'];
+
+const encodeXML = function (str) {
+    return str
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&apos;');
+};
+
+const axiosConfig = {
+    headers: {
+        'Ocp-Apim-Subscription-Key': config.VALIDATOR_SERVICES_API_KEY,
+    },
+};
+
+const siteUrl = config.VALIDATOR_FRONTEND_URL;
+const publisherUrl = `${config.VALIDATOR_SERVICES_API_URL}pvt/publishers`;
+
+const getPublishers = async () => {
+    const publishers = await axios.get(publisherUrl, axiosConfig).then((result) => result.data);
+    return publishers;
+};
+
+const getSitemap = async () => {
+    let sitemapString =
+        '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">';
+    const publishers = await getPublishers();
+    sitemapString += publishers
+        .map(
+            (d) =>
+                `<url><loc>${siteUrl}organisation/${encodeXML(
+                    encodeURIComponent(d.name)
+                )}</loc></url>`
+        )
+        .join('');
+    sitemapString += additionalPages
+        .map((d) => `<url><loc>${siteUrl}${encodeXML(encodeURIComponent(d))}</loc></url>`)
+        .join('');
+    sitemapString += '</urlset>';
+    return sitemapString;
+};
+
+module.exports = async (context) => {
+    const responseMessage = await getSitemap();
+
+    context.res = {
+        status: 200,
+        headers: { 'Content-Type': 'application/xml' },
+        body: responseMessage,
+    };
+};
